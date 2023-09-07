7243e885cc485d6088c8733f411ec82060cf524d
idyharrisonn <idyharrison20@gmail.com>
2023-09-07 15:02:18 -0700

    jug

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main -it prints out output using put
+ *
+ * Return: always (0)
+ *
+ */
+int main(void)
+{
+puts ("\"programming is like building a multilingual puzzle")
+	Return(0)
+}	
