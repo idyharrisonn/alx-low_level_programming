ec2123b2369980244c07f7d28bf19d63fd13625f
idyharrisonn <idyharrison20@gmail.com>
2023-09-11 17:13:20 -0700

    slow

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,40 +1,49 @@
 #include <stdio.h>
 /**
- * main - main fuction
+ * main - main function
  *
  * Return: always 0
  */
 int main(void)
 {
-	int num1, num2, num3, num4;
+	int num1 = 0;
+	int num2 = 0;
+	int num3 = 0;
+	int num4 = 0;
 
-	for (num1 = 0; num1 <= 9; num1++)
-    {
-	    for (num2 = 0; num2 <= 9; num2++)
+	while (num1 <= '9')
 	{
-		for (num3 = 0; num3 <= 9; num3++)
-	    {
-		    for (num4 = 0; num4 <= 9; num4++)
+		while (num2 <= '9')
 		{
-			if ((num3 + num4 > num1 + num2 && num3 >= num1) || num1 < num3)
-		    {
-			    putchar(num1 + '0');
-			putchar(num2 + '0');
-			putchar(' ');
-			putchar(num3 + '0');
-			putchar(num4 + '0');
-			if (num1 + num2 + num3 + num4 == 17 && num1 == 9)
+			while (num3 <= '9')
+			{
+				while (num4 <= '9')
+				{
+					if (((num3 + num4) > (num1 + num2) && num3 >= num1) || num1 < num3)
+					{
+						putchar(num1);
+						putchar(num2);
+						putchar(' ');
+						putchar(num3);
+						putchar(num4);
+
+		if (num1 == '9' && num2 + num3 + num4 == 179)
 			{
 				break;
-			    else
+			}
+			else
 			{
 				putchar(',');
-			    putchar(' ')
+				putchar(' ');
 			}
-		    }
+					}
+				}
+				num3++;
+			}
+			num2++;
 		}
-	    }
+		num1++;
 	}
-    }
-    return (0);
+	putchar('\n');
+	return (0);
 }
