3c2c0bfaa856d9fb4ec118ec900c758537630a2e
idyharrisonn <idyharrison20@gmail.com>
2023-09-15 21:14:16 -0700

    stroll

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,13 +7,13 @@
 void print_line(int n)
 {
 	if (n <= 0)
-		_putchar('\n');
+	_putchar('\n');
 	else
 	{
-		int i;
+	int i;
 
-		for (i = 1; i <= n; i++)
-			_putchar('_');
-		_putchar('\n');
+	for (i = 1; i <= n; i++)
+	_putchar('_');
+	_putchar('\n');
 	}
 }
