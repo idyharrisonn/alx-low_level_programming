f2dd0ab6b2f7b3b00fd738e7a0b36066de3be5b0
idyharrisonn <idyharrison20@gmail.com>
2023-09-26 18:10:33 -0700

    calculator

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -8,11 +8,11 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int i, n, sum1 = 0, sum2 = 0;
+	int i, sum1 = 0, sum2 = 0;
 
-	for (i = 0; i <= (size * size); i = i + size + 1)
+	for (i = 0; i < size * size; i = i + size + 1)
 		sum1 = sum1 + a[i];
-	for (n = size - 1; n <= (size * size) - size; n = n + size - 1)
+	for (i = size - 1; i < size * size - size + 1; i = i + size - 1)
 		sum2 = sum2 + a[i];
 	printf("%d, %d\n", sum1, sum2);
 }
