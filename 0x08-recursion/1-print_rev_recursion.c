f25792a8b1a4286e1b052622470cd7c86aec9a82
idyharrisonn <idyharrison20@gmail.com>
2023-09-28 13:46:45 -0700

    scratch

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - print strings in reverse
+ * @s: string on reverse
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+	else
+		_putchar('\n');
+}
