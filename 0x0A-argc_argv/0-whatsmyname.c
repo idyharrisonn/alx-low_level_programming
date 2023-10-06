4a58877040559df033749634384bf74af3c14bac
idyharrisonn <idyharrison20@gmail.com>
2023-10-06 12:06:43 -0700

    period

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *
+ * main - prints its name followed by a new line
+ * @argc: number of command line arguement
+ * @argv: array that contain the command line arguement
+ * Return: (0) when successful
+ */
+int main(int argc __attribute__ ((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
