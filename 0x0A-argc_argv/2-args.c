b1dbaa77632e022d077319beac28fb49b19a94ea
idyharrisonn <idyharrison20@gmail.com>
2023-10-07 09:07:05 -0700

    food

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print all arguement it receives
+ * @argc: number of command line arguement
+ * @argv: array that contains the program command line arg
+ * Return: (0) when successful
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
