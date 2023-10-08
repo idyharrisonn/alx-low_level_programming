a2baee605c5fc0ca22fa3518f2b9af2c1b4f8935
idyharrisonn <idyharrison20@gmail.com>
2023-10-08 15:52:58 -0700

    please work

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -9,28 +9,38 @@
  */
 int main(int argc, char **argv)
 {
-	int cents, mncoin = 0;
+	int cents = atoi(argv[1]);
+	int coins = 0;
 
-	if (argc == 1 || argc > 2)
+	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	cents = atoi(argv[1]);
 	while (cents > 0)
 	{
 		if (cents >= 25)
+		{
 			cents -= 25;
-		if (cents >= 10)
+		}
+		else if (cents >= 10)
+		{
 			cents -= 10;
-		if (cents >= 5)
+		}
+		else if (cents >= 5)
+		{
 			cents -= 5;
-		if (cents >= 2)
+		}
+		else if (cents >= 2)
+		{
 			cents -= 2;
-		if (cents >= 1)
+		}
+		else if (cents >= 1)
+		{
 			cents -= 1;
-		mncoin += 1;
+		}
+		coins++;
 	}
-	printf("%d\n", mncoin);
+	printf("%d\n", coins);
 	return (0);
 }
