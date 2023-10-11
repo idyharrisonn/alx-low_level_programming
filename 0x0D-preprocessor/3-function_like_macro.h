e5dcd809af4fd50e3f43013c71c451fdf6656f96
idyharrisonn <idyharrison20@gmail.com>
2023-10-10 18:27:56 -0700

    fish

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,10 @@
+#ifndef MACROLIKE_H
+#define MACROLIKE_H
+/**
+ * 3-function_like_macro.h
+ * @x: input value (an int)
+ *
+ * Desc: header file that computes absolute value
+ */
+#define ABS(x) ((x) < 0 ? -(1) * (x) : (x))
+#endif
