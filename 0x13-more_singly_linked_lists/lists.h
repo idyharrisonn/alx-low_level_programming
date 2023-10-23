289271860a06639feb5931635951f18e9cec6ffc
idyharrisonn <idyharrison20@gmail.com>
2023-10-23 04:44:20 -0700

    cream

---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,18 @@
+#ifndef _LIST_H_
+#define _LIST_H_
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * struct listint_s - selr referential struct
+ * for creating nodes of a singly linked list
+ * @n: integer data
+ * @next: self referential pointer
+ * pointer to the next node of a list
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+size_t print_listint(const listint_t *h);
+#endif
